6f36f4dba30130dcab8e437f430a3071db46d11c
irvinzato <multizato@hotmail.com>
2022-08-28 00:50:32 -0500

    Agregue tercer parametro con try catch por si no es enviado

--- src/main/java/org/rivera/webapp/servlet/ParametroGetServlet.java
+++ src/main/java/org/rivera/webapp/servlet/ParametroGetServlet.java
@@ -18,6 +18,8 @@ public class ParametroGetServlet extends HttpServlet {
     PrintWriter out = resp.getWriter();
 
     String hi = req.getParameter("hi");
+    String name = req.getParameter("name");
+
     out.println("<!DOCTYPE html>");
     out.println("<html>");
     out.println("   <head>");
@@ -26,7 +28,22 @@ public class ParametroGetServlet extends HttpServlet {
     out.println("   </head>");
     out.println("   <body>");
     out.println("     <h1> Jakarta - Parámetros Get de la url </h1>");
-    out.println("     <h2> El saludo enviado es: " + hi +  "</h2>");
+    if( hi != null && name != null ) {
+      out.println("     <h2> El saludo enviado es: " + hi + " " + name + "</h2>");
+    } else if( hi != null ) {
+      out.println("     <h2> El saludo enviado es: " + hi + "</h2>");
+    } else if( name != null ) {
+      out.println("     <h2> El nombre es: " + name + "</h2>");
+    } else {
+      out.println("     <h2> No se pasaron parámetros </h2>");
+    }
+    try {
+      Integer code = Integer.parseInt(req.getParameter("code"));
+      out.println("     <h3> El código enviado: " + code + "</h3>");
+    } catch(NumberFormatException e) {
+      out.println("     <h3> No se ha enviado código </h3>");
+    }
+
     out.println("   </body>");
     out.println("</html>");
     out.close();
